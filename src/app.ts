a83bda8a9d56c4d1acce38cb23dfa9b471755ed7
Dimas Vega <dimas_vga@hotmail.com>
2023-07-18 17:23:34 -0600

    adding new message in app.ts

--- src/app.ts
+++ src/app.ts
@@ -13,5 +13,5 @@ app.use(cors())
 app.use(express.json());
 app.use(router);
 db().then(() => console.log("Conexion ready to use, even database"));
-
+console.log('Proceso casi finalizado');
 app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
